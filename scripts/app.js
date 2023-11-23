83dd8c002298700547d53a54137c5ace14a3a4eb
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-11-23 16:02:57 +0100

    add aria-* and focus with javascript

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('Script is working!!!');
+// Get elements from the DOM
+const menuBtn = document.getElementById('menubutton');
+
+// Add event listeners
+menuBtn.addEventListener('click', toggleMenu);
+document.addEventListener('keypress', closeMenu);
+
+// Function to execute
+function toggleMenu() {
+  document.getElementById('menu-content').classList.toggle('menu-open');
+
+  // change the aria-* attribute
+  // Focus on first menu item or button
+  if (menuBtn.attributes['aria-expanded'].value === 'true') {
+    menuBtn.ariaExpanded = 'false';
+    menuBtn.focus();
+  } else {
+    menuBtn.ariaExpanded = 'true';
+    const menuItems = document.querySelectorAll('[role="menuitem"]');
+    menuItems.item(0).focus();
+  }
+}
+
+function closeMenu() {}
